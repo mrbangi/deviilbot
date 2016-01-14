227518061e2b286b960a7b06d0dc583bbbbdfb5d
mrbangi <xboxlivemnasr@gmail.com>
2016-01-14 14:35:37 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -229,8 +229,9 @@ function create_config( )
     "info",
     "feedback",
     "echo",
+    "tex",
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {164059631,tonumber(164059631)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
